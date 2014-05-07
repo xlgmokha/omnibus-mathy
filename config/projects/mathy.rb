4d889c058bbc80106749dffc592320c32bcf9571
mo khan <mo@mokhan.ca>
2014-05-06 21:20:33 -0600

    create omnibus.

---
+++ config/projects/mathy.rb
@@ -0,0 +1,21 @@
+
+name 'mathy'
+maintainer 'CHANGE ME'
+homepage 'CHANGEME.com'
+
+replaces        'mathy'
+install_path    '/opt/mathy'
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+# creates required build directories
+dependency 'preparation'
+
+# mathy dependencies/components
+# dependency 'somedep'
+
+# version manifest file
+dependency 'version-manifest'
+
+exclude '\.git*'
+exclude 'bundler\/git'
